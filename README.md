769a4f3fa2c9392f2d3dd0d6b1dab79e9890d4a0
Brooke M. Fujita <buruzaemon@users.noreply.github.com>
2010-12-30 15:14:00 +0900

    Adding support for all-morphs and partial options. Further updating of documentation with markdown.
    user: buruzaemon <buruzaemon@gmail.com>
    branch 'default'
    updated .yardopts
    updated LICENSE
    updated README.md
    on-going development of lib/natto.rb
    on-going development of lib/natto/binding.rb
    updated test/test_natto.rb

--- README.md
+++ README.md
@@ -1,27 +1,31 @@
-# natto: A Tasty Ruby Binding with MeCab
+# natto
+A Tasty Ruby Binding with MeCab
 
 ## What is natto?
 
-natto provides a Ruby binding with MeCab, 
-the part-of-speech and morphological analyzer 
-for the Japanese language.
+natto combines the [Ruby programming language](http://www.ruby-lang.org/) with [MeCab](http://mecab.sourceforge.net/), the part-of-speech and morphological analyzer for the Japanese language.
 
-## Try It! Try It!
-
-### Requirements
+## Requirements
 natto requires the following:
 
-* [MeCab 0.98](http://sourceforge.net/projects/mecab/files/mecab/)
-* [ffi 0.6.3 or greater](http://rubygems.org/gems/ffi)
-* Ruby 1.8.7 or greater
+-  [MeCab _0.98_](http://sourceforge.net/projects/mecab/files/mecab/0.98/)
+-  [ffi _0.6.3 or greater_](http://rubygems.org/gems/ffi)
+-  Ruby _1.8.7 or greater_
 
-### Installation
+## Installation
 Install natto with the following gem command:
     gem install natto
 
-### Configuration
-* natto will try to locate the <tt>mecab</tt> library based upon its runtime environment.
-* In case of <tt>LoadError</tt>, please set the <tt>MECAB_PATH</tt> environment variable to the exact name/path to your <tt>mecab</tt> library.
+## Configuration
+-  natto will try to locate the <tt>mecab</tt> library based upon its runtime environment.
+-  In case of <tt>LoadError</tt>, please set the <tt>MECAB_PATH</tt> environment variable to the exact name/path to your <tt>mecab</tt> library.
+
+e.g., for bash on UNIX/Linux
+    export MECAB_PATH=mecab.so
+e.g., on Windows
+    set MECAB_PATH=C:\Program Files\MeCab\bin\libmecab.dll
+e.g., for Cygwin
+    export MECAB_PATH=cygmecab-1
 
 ## Usage
     require 'natto'
@@ -39,39 +43,42 @@ Install natto with the following gem command:
     => nil
 
 ## Contributing to natto
-* Check out the latest master to make sure the feature hasn't been implemented or the bug hasn't been fixed yet
-* Check out the issue tracker to make sure someone already hasn't requested it and/or contributed it
-* Fork the project
-* Start a feature/bugfix branch
-* Commit and push until you are happy with your contribution
-* Make sure to add tests for it. This is important so I don't break it in a future version unintentionally.
-* Please try not to mess with the Rakefile, version, or history. If you must have your own version, that is fine, but please isolate to its own commit so I can cherry-pick around it.
+-  Use [Mercurial](http://mercurial.selenic.com/) and [check out the latest master](http://code.google.com/p/natto/source/checkout) to make sure the feature hasn't been implemented or the bug hasn't been fixed yet.
+-  [Check out the issue tracker](http://code.google.com/p/natto/issues/list) to make sure someone already hasn't requested it and/or contributed it.
+-  Fork the project.
+-  Start a feature/bugfix branch.
+-  Commit and push until you are happy with your contribution.
+-  Make sure to add tests for it. This is important so I don't break it in a future version unintentionally. I use [Test::Unit](http://ruby-doc.org/stdlib/libdoc/test/unit/rdoc/classes/Test/Unit.html) since it is simple and it works.
+-  Please try not to mess with the Rakefile, version, or history. If you must have your own version, that is fine, but please isolate to its own commit so I can cherry-pick around it.
 
 ## Changelog
 
-- **2010/12/28**: 0.0.6 release.
-    - Correction to natto.gemspec to include lib/natto/binding.rb.
+- __2010/12/30: 0.0.7 release.
+    - Adding support for all-morphs and partial options
+    - Further updating of documentation with markdown
 
-- **2010/12/28**: 0.0.5 release. (yanked)
+- __2010/12/28__: 0.0.6 release.
+    - Correction to natto.gemspec to include lib/natto/binding.rb
+
+- __2010/12/28__: 0.0.5 release. (yanked)
     - On-going refactoring
     - Project structure refactored for greater maintainability
 
-- **2010/12/26**: 0.0.4 release.
+- __2010/12/26__: 0.0.4 release.
     - On-going refactoring
 
-- **2010/12/23**: 0.0.3 release.
+- __2010/12/23__: 0.0.3 release.
     - On-going refactoring
     - Adding documentation via yard
 
-- **2010/12/20**: 0.0.2 release.
+- __2010/12/20__: 0.0.2 release.
     - Continuing development on proper resource deallocation
     - Adding options hash in object initializer 
 
-- **2010/12/13**: Released version 0.0.1. The objective is to provide
+- __2010/12/13__: Released version 0.0.1. The objective is to provide
   an easy-to-use, production-level Ruby binding to MeCab.
     - Initial release 
 
-
 ## Copyright
 
-natto (c) 2010-2013 by Brooke M. Fujita, licensed under the new BSD license. Please see the (file:LICENSE) document for further details.
+natto &copy; 2010-2013 by Brooke M. Fujita, licensed under the new BSD license. Please see the [LICENSE](file.LICENSE.html) document for further details.
--- lib/natto.rb
+++ lib/natto.rb
@@ -1,85 +1,85 @@
-# -*- encoding: utf-8 -*-
 require 'rubygems' if RUBY_VERSION.to_f < 1.9
 require 'natto/binding'
 
-# natto combines the Ruby programming language with MeCab, 
-# the part-of-speech and morphological analyzer for the
-# Japanese language.
-# 
-# === Requirements
-# natto requires the following:
-# * {http://sourceforge.net/projects/mecab/files/mecab/ MeCab 0.98}
-# * {http://rubygems.org/gems/ffi ffi 0.63 or greater}
-# * Ruby 1.8.7 or greater
-#
-# === Installation
-# Install natto with the following gem command:
-# * <code>gem install natto</code>
-#
-# === Configuration
-# * natto will try to locate the <tt>mecab</tt> library based upon its runtime environment.
-# * In case of <tt>LoadError</tt>, please set the <tt>MECAB_PATH</tt> environment variable to the exact name/path to your <tt>mecab</tt> library.
-#
-#== Usage
-#  require 'natto'
-#
-#  m = Natto::MeCab.new
-#  puts m.parse("すもももももももものうち")
-#  すもも  名詞,一般,*,*,*,*,すもも,スモモ,スモモ
-#  も      助詞,係助詞,*,*,*,*,も,モ,モ
-#  もも    名詞,一般,*,*,*,*,もも,モモ,モモ
-#  も      助詞,係助詞,*,*,*,*,も,モ,モ
-#  もも    名詞,一般,*,*,*,*,もも,モモ,モモ
-#  の      助詞,連体化,*,*,*,*,の,ノ,ノ
-#  うち    名詞,非自立,副詞可能,*,*,*,うち,ウチ,ウチ
-#  EOS
-#  => nil
-#
-# @author Brooke M. Fujita (buruzaemon)
 module Natto 
   require 'ffi'
 
-  # <tt>MeCab</tt> is a wrapper class to the <tt>mecab</tt> parser.
-  # Options to the <tt>mecab</tt> parser are passed in as a hash to
-  # #initialize.
+  # <tt>MeCab</tt> is a wrapper class for the <tt>mecab</tt> parser.
+  # Options to the <tt>mecab</tt> parser are passed in as a hash at
+  # initialization.
+  #
+  # ## Usage
+  #     require 'natto'
+  #
+  #     m = Natto::MeCab.new
+  #     puts m.parse("すもももももももものうち")
+  #     すもも  名詞,一般,*,*,*,*,すもも,スモモ,スモモ
+  #     も      助詞,係助詞,*,*,*,*,も,モ,モ
+  #     もも    名詞,一般,*,*,*,*,もも,モモ,モモ
+  #     も      助詞,係助詞,*,*,*,*,も,モ,モ
+  #     もも    名詞,一般,*,*,*,*,もも,モモ,モモ
+  #     の      助詞,連体化,*,*,*,*,の,ノ,ノ
+  #     うち    名詞,非自立,副詞可能,*,*,*,うち,ウチ,ウチ
+  #     EOS
+  #     => nil
   # 
-  # @see {SUPPORTED_OPTS}
+  # @see SUPPORTED_OPTS
   class MeCab
+
+    attr_reader :options
+
     # Supported options to the <tt>mecab</tt> parser.
     # See the <tt>mecab</tt> help for more details. 
-    SUPPORTED_OPTS = [ :rcfile, :dicdir, :userdic, :output_format_type, :lattice_level, 
-                       :node_format, :unk_format, :bos_format, :eos_format, :eon_format,
-                       :unk_feature, :nbest, :theta, :cost_factor ].freeze
-                       # :all_morphs, :partial, :allocate_sentence ]
+    SUPPORTED_OPTS = [  :rcfile, :dicdir, :userdic, :lattice_level, :all_morphs,
+                        :output_format_type, :partial, :node_format, :unk_format, 
+                        :bos_format, :eos_format, :eon_format, :unk_feature, 
+                        :nbest, :theta, :cost_factor ].freeze
+                        # :allocate_sentence ]
 
-    # Initialize the wrapped <tt>mecab</tt> instance, with the
+    #OPTION_DEFAULTS = { :lattice_level=>0, :all_morphs=>false, :nbest=>1, 
+    #                    :theta=>0.75, :cost_factor=>700 }.freeze
+
+    # Initializes the wrapped <tt>mecab</tt> instance with the
     # given <tt>options</tt> hash.
-    # <br/>
+    # 
     # Options supported are:
-    # * :rcfile -- resource file
-    # * :dicdir -- system dicdir
-    # * :userdic -- user dictionary
-    # * :lattice_level -- lattice information level (integer, default 0)
-    # * :output_format_type -- output format type (wakati, chasen, yomi, etc.)
-    # * :node_format -- user-defined node format
-    # * :unk_format -- user-defined unknown node format
-    # * :bos_format -- user-defined beginning-of-sentence format
-    # * :eos_format -- user-defined end-of-sentence format
-    # * :eon_format -- user-defined end-of-NBest format
-    # * :unk_feature -- feature for unknown word
-    # * :nbest --  output N best results (integer, default 1)
-    # * :theta -- temperature parameter theta (float, default 0.75)
-    # * :cost_factor -- cost factor (integer, default 700)
-    # <br/>
-    # Use single-quotes to preserve format options that contain escape chars.
-    # <br/>
+    #
+    # - :rcfile --  resource file
+    # - :dicdir --  system dicdir
+    # - :userdic --  user dictionary
+    # - :lattice_level --  lattice information level (integer, default 0)
+    # - :all_morphs --  output all morphs (default false)
+    # - :output_format_type --  output format type (wakati, chasen, yomi, etc.)
+    # - :partial --  partial parsing mode
+    # - :node_format --  user-defined node format
+    # - :unk_format --  user-defined unknown node format
+    # - :bos_format --  user-defined beginning-of-sentence format
+    # - :eos_format --  user-defined end-of-sentence format
+    # - :eon_format --  user-defined end-of-NBest format
+    # - :unk_feature --  feature for unknown word
+    # - :nbest --  output N best results (integer, default 1)
+    # - :theta --  temperature parameter theta (float, default 0.75)
+    # - :cost_factor --  cost factor (integer, default 700)
+    # 
+    # _Use single-quotes to preserve format options that contain escape chars._
+    # 
     # e.g.
-    #   m = Natto::MeCab.new(:node_format=>'%m\t%f[7]\n')
+    #     m = Natto::MeCab.new(:node_format=>'%m\t%f[7]\n')
+    #     puts m.parse("日本語は難しいです。")
+    #     日本語  ニホンゴ
+    #     は      ハ
+    #     難しい  ムズカシイ
+    #     です    デス
+    #     。      。
+    #     EOS
+    #     => nil
     #
     # @param [Hash]
-    # @see {SUPPORTED_OPTS}
+    # @raise [MeCabError] if <tt>mecab</tt> cannot be initialized with the given <tt>options</tt>
+    # @see MeCab::SUPPORTED_OPTS
     def initialize(options={})
-      opt_str = self.class.build_options_str(options)
+      @options = options
+      opt_str = self.class.build_options_str(@options)
       @ptr = Natto::Binding.mecab_new2(opt_str)
       raise MeCabError.new("Could not initialize MeCab with options: '#{opt_str}'") if @ptr.address == 0
       #@dict = Natto::DictionaryInfo.new(Natto::Binding.mecab_dictionary_info(@ptr))
@@ -88,7 +88,9 @@ module Natto
 
     # Parses the given string <tt>s</tt>.
     #
-    # @param [String]
+    # @param [String] s
+    # @return parsing result from <tt>mecab</tt>
+    # @raise [MeCabError] if the <tt>mecab</tt> parser cannot parse the given string <tt>s</tt>
     def parse(s)
       Natto::Binding.mecab_sparse_tostr(@ptr, s) || 
         raise(MeCabError.new(Natto::Binding.mecab_strerror(@ptr)))
@@ -99,6 +101,7 @@ module Natto
     # after the object owning <tt>ptr</tt> has been destroyed.
     #
     # @param [FFI::MemoryPointer] ptr
+    # @return [Proc] to release <tt>mecab</tt> resources properly
     def self.create_free_proc(ptr)
       Proc.new do
         Natto::Binding.mecab_destroy(ptr)
@@ -109,12 +112,17 @@ module Natto
     # be passed in the construction of <tt>mecab</tt>.
     #
     # @param [Hash] options 
+    # @return string-representation of the options to the <tt>mecab</tt> parser
     def self.build_options_str(options={})
       opt = []
       SUPPORTED_OPTS.each do |k|
         if options.has_key? k
           key = k.to_s.gsub('_', '-')  
-          opt << "--#{key}=#{options[k]}"
+          if %w( all-morphs partial ).include? key
+            opt << "--#{key}" if options[k]==true
+          else
+            opt << "--#{key}=#{options[k]}"
+          end
 
           #if key.end_with? '_format_' or key.end_with? '_feature'
           #  opt << "--#{key}="+options[k]
@@ -133,24 +141,26 @@ module Natto
 
   # <tt>DictionaryInfo</tt> is a wrapper for a <tt>MeCab</tt>
   # instance's related dictionary information.
-  # <br>
+  # 
   # Values may be obtained by using the following symbols 
   # as keys to the hash of <tt>mecab</tt> dictionary information.
-  # * :filename
-  # * :charset
-  # * :size
-  # * :type
-  # * :lsize
-  # * :rsize
-  # * :version
-  # * :next
-  # <br>
-  # Usage:
-  #  dict = Natto::DictionaryInfo.new(mecab_ptr)
-  #  puts dict[:filename]
-  #  =>  /usr/local/lib/mecab/dic/ipadic/sys.dic
-  #  puts dict[:charset]
-  #  =>  utf8
+  #
+  # - :filename
+  # - :charset
+  # - :size
+  # - :type
+  # - :lsize
+  # - :rsize
+  # - :version
+  # - :next
+  # 
+  # # Usage:
+  #
+  #     dict = Natto::DictionaryInfo.new(mecab_ptr)
+  #     puts dict[:filename]
+  #     =>  /usr/local/lib/mecab/dic/ipadic/sys.dic
+  #     puts dict[:charset]
+  #     =>  utf8
   class DictionaryInfo < FFI::Struct
     layout  :filename, :string,
             :charset,  :string,
