e0d11e2307ee862bb268ecdc94518f4acfc035a9
Brooke M. Fujita <buruzaemon@users.noreply.github.com>
2011-01-06 18:57:42 +0900

    On-going dev. user: buruzaemon <buruzaemon@gmail.com> branch 'default' changed README.md changed lib/natto.rb changed lib/natto/binding.rb changed lib/natto/version.rb changed natto.gemspec

--- lib/natto.rb
+++ lib/natto.rb
@@ -1,6 +1,37 @@
+# coding: utf-8
 require 'rubygems' if RUBY_VERSION.to_f < 1.9
 require 'natto/binding'
 
+# natto combines the Ruby programming language with MeCab, 
+# the part-of-speech and morphological analyzer for the
+# Japanese language.
+# 
+# ## Requirements
+# natto requires the following:
+#
+# -  [MeCab _0.98_](http://sourceforge.net/projects/mecab/files/mecab/0.98/)
+# -  [ffi _0.6.3 or greater_](http://rubygems.org/gems/ffi)
+# -  Ruby _1.8.7 or greater_
+#
+# ## Installation
+# Install natto with the following gem command:
+#     gem install natto
+#
+# ## Configuration
+# - natto will try to locate the <tt>mecab</tt> library based upon its runtime environment.
+# - In case of <tt>LoadError</tt>, please set the <tt>MECAB_PATH</tt> environment variable to the exact name/path to your <tt>mecab</tt> library.
+#
+#  e.g., for bash on UNIX/Linux
+#       export MECAB_PATH=mecab.so
+#  e.g., on Windows
+#       set MECAB_PATH=C:\Program Files\MeCab\bin\libmecab.dll
+#  e.g., for Cygwin
+#       export MECAB_PATH=cygmecab-1
+#
+# ## Usage
+# hi すもも  名詞,一般,*,*,*,*,すもも,スモモ,スモモ
+#     &amp;#x65e5;
+#
 module Natto 
   require 'ffi'
 
@@ -13,17 +44,16 @@ module Natto
   #
   #     m = Natto::MeCab.new
   #     puts m.parse("すもももももももものうち")
-  #     すもも  名詞,一般,*,*,*,*,すもも,スモモ,スモモ
-  #     も      助詞,係助詞,*,*,*,*,も,モ,モ
-  #     もも    名詞,一般,*,*,*,*,もも,モモ,モモ
-  #     も      助詞,係助詞,*,*,*,*,も,モ,モ
-  #     もも    名詞,一般,*,*,*,*,もも,モモ,モモ
-  #     の      助詞,連体化,*,*,*,*,の,ノ,ノ
-  #     うち    名詞,非自立,副詞可能,*,*,*,うち,ウチ,ウチ
-  #     EOS
+  #     => すもも  名詞,一般,*,*,*,*,すもも,スモモ,スモモ
+  #     => も      助詞,係助詞,*,*,*,*,も,モ,モ
+  #     => もも    名詞,一般,*,*,*,*,もも,モモ,モモ
+  #     => も      助詞,係助詞,*,*,*,*,も,モ,モ
+  #     => もも    名詞,一般,*,*,*,*,もも,モモ,モモ
+  #     => の      助詞,連体化,*,*,*,*,の,ノ,ノ
+  #     => うち    名詞,非自立,副詞可能,*,*,*,うち,ウチ,ウチ
+  #     => EOS
   #     => nil
   # 
-  # @see SUPPORTED_OPTS
   class MeCab
 
     attr_reader :options
