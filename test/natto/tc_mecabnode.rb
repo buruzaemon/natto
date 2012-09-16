2dae8b5efca681fcd45e355adb2df4da124cd094
Brooke M. Fujita <buruzaemon@users.noreply.github.com>
2012-09-16 21:48:47 +0900

    Changes and refactorings related to Issue 9 fix.
    user: buruzaemon
    branch 'default'
    added lib/natto/option_parse.rb
    added test/natto/tc_option_parse.rb
    changed CHANGELOG
    changed README.md
    changed lib/natto.rb
    changed lib/natto/version.rb
    changed natto.gemspec
    changed test/natto/tc_mecab.rb
    changed test/natto/tc_mecabnode.rb
    changed test/test_natto.rb

--- test/natto/tc_mecabnode.rb
+++ test/natto/tc_mecabnode.rb
@@ -32,7 +32,7 @@ class TestMeCabNode < Test::Unit::TestCase
   def test_surface_and_feature_accessors
     raw = `#{@test_cmd} | mecab`.lines.to_a
     raw.delete_if {|e| e =~ /^(EOS|BOS|\t)/ }
-    raw.map!{|e| e.force_encoding(Encoding.default_external)} if @host_os =~ /mswin|mingw/i && @arch =~ /java/i && RUBY_VERSION.to_f >= 1.9
+    raw.map!{|e| e.force_encoding(Encoding.default_external)} if @arch =~ /java/i && RUBY_VERSION.to_f >= 1.9
     expected = {}
     raw.each do |l|
       tokens = l.split("\t")
