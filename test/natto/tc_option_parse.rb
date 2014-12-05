679a9df46322a5fb15fbe0219f0849aab31619a8
Brooke M. Fujita <buruzaemon@users.noreply.github.com>
2014-12-06 00:19:12 +0900

    Updating LICENSE, initial work for Issue 14

--- test/natto/tc_option_parse.rb
+++ test/natto/tc_option_parse.rb
@@ -1,7 +1,6 @@
 # coding: utf-8
 
-#class TestNattoOptParse < Test::Unit::TestCase
-class TestNattoOptParse < MiniTest::Unit::TestCase
+class TestOptionParse < MiniTest::Unit::TestCase
   def setup
     @klass = Class.new do
       include Natto::OptionParse
@@ -203,3 +202,29 @@ class TestNattoOptParse < MiniTest::Unit::TestCase
     assert_equal('--cost-factor=42', @klass.build_options_str(:cost_factor=>42))
   end
 end
+
+# Copyright (c) 2014, Brooke M. Fujita.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+#  * Redistributions of source code must retain the above
+#    copyright notice, this list of conditions and the
+#    following disclaimer.
+# 
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the
+#    following disclaimer in the documentation and/or other
+#    materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
